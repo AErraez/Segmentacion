54458803f040b64f4b5bc1e8d864dc6f904531c1
AErraez <122416413+AErraez@users.noreply.github.com>
2025-10-02 17:17:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ boton.addEventListener("click", () => {
       let buttonsHtml = similist
         .map(
           (s) =>
-            `<div class="col">
+            `<div class="col-2">
               <button class="contratante-but btn btn-outline-primary w-100" 
                 data-line="${idx}" 
                 data-sistema="${s.SISTEMA}" 
@@ -73,7 +73,7 @@ boton.addEventListener("click", () => {
         .join("");
 
       let searchHtml = `
-        <div class="col-4">
+        <div class="col-3">
           <input type="text" class="form-control mini-search" autocomplete="off" 
             placeholder="Buscar manualmente..." 
             data-line="${idx}">
@@ -83,7 +83,7 @@ boton.addEventListener("click", () => {
 
       let rowHtml = `
         <div class="row py-2 align-items-center" data-line="${idx}">
-          <div class="col-3"><strong>${element}</strong> ⇒ </div>
+          <div class="col-3">${element} ⇒ </div>
           ${buttonsHtml}
           ${searchHtml}
         </div>`;
@@ -293,3 +293,4 @@ function autoActivateAboveThreshold() {
     }
   });
 }
+
